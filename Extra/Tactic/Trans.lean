0137af79a2a05dceeea9ac17301349b784451509
François G. Dorais <fgdorais@gmail.com>
2023-11-23 06:17:28 -0500

    chore: style

--- Extra/Tactic/Trans.lean
+++ Extra/Tactic/Trans.lean
@@ -5,6 +5,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 
 syntax "trans " term:max ("using " term:max (", " term:max)?)? : tactic
 macro_rules
-| `(tactic|trans $b) => `(tactic|apply Trans.trans (b:=$b))
-| `(tactic|trans $b using $r) => `(tactic|apply Trans.trans (r:=$r) (s:=$r) (b:=$b))
-| `(tactic|trans $b using $r, $s) => `(tactic|apply .trans (r:=$r) (s:=$s) (b:=$b))
+  | `(tactic| trans $b) => `(tactic| apply Trans.trans (b:=$b))
+  | `(tactic| trans $b using $r) => `(tactic| apply Trans.trans (r:=$r) (s:=$r) (b:=$b))
+  | `(tactic| trans $b using $r, $s) => `(tactic| apply .trans (r:=$r) (s:=$s) (b:=$b))
