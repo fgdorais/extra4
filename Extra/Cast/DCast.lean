bea33eac465de3119045978c28c53c2b16f6f548
François G. Dorais <fgdorais@gmail.com>
2023-11-23 05:53:38 -0500

    feat: add congr_dcast (#3)
    * feat: add congr_dcast
    
    * chore: style

--- Extra/Cast/DCast.lean
+++ Extra/Cast/DCast.lean
@@ -5,15 +5,23 @@ Released under Apache 2.0 license as described in the file LICENSE.
 import Extra.Basic
 import Extra.Cast.DEq
 
+@[nolint simpVarHead, simp]
+theorem congr_ndrec {β : α → Sort _} (f : (x : α) → β x → γ) (h : x = x') (y : β x) :
+    f x' (Eq.ndrec y h) = f x y := by cases h; rfl
+
 /-- Dependent type casting -/
 def dcast {motive : α → Sort _} (h : a = b) (t : motive a) : motive b := Eq.ndrec t h
+
+@[elim_cast]
+theorem cast_eq_dcast (h : α = β) (t : α) : cast h t = dcast (motive:=id) h t := rfl
 
 @[elim_cast]
 theorem Eq.ndrec_eq_dcast {motive : α → Sort _} (h : a = b) (t : motive a) :
     Eq.ndrec t h = dcast h t := rfl
 
-@[elim_cast]
-theorem cast_eq_dcast (h : α = β) (t : α) : cast h t = dcast (motive:=id) h t := rfl
+@[simp, elim_cast]
+theorem congr_dcast {β : α → Sort _} (f : (x : α) → β x → γ) (h : x = x') (y : β x) :
+    f x' (dcast h y) = f x y := congr_ndrec ..
 
 @[simp, elim_cast]
 theorem dcast_id {motive : α → Sort _} (h : a = a) (t : motive a) : dcast h t = t := by cases h; rfl
@@ -24,17 +32,16 @@ theorem dcast_comp {motive : α → Sort _} (hab : a = b) (hbc : b = c) (t : mot
 
 @[simp, elim_cast]
 theorem deq_dcast_left {motive : α → Sort _} (h : a = b) (t : motive a) (u : motive c) :
-    DEq (dcast h t) u ↔ DEq t u := by cases h; exact Iff.rfl
+    DEq (dcast h t) u ↔ DEq t u := by cases h; rfl
 
 @[simp, elim_cast]
 theorem deq_dcast_right {motive : α → Sort _} (h : b = c) (t : motive a) (u : motive b) :
-    DEq t (dcast h u) ↔ DEq t u := by cases h; exact Iff.rfl
+    DEq t (dcast h u) ↔ DEq t u := by cases h; rfl
 
 @[simp, elim_cast]
 theorem heq_dcast_left {motive : α → Sort _} (h : a = b) (t : motive a) (u : motive c) :
-    HEq (dcast h t) u ↔ HEq t u := by
-  cases h; exact Iff.rfl
+    HEq (dcast h t) u ↔ HEq t u := by cases h; rfl
 
 @[simp, elim_cast]
 theorem heq_dcast_right {motive : α → Sort _} (h : b = c) (t : motive a) (u : motive b) :
-    HEq t (dcast h u) ↔ HEq t u := by cases h; exact Iff.rfl
+    HEq t (dcast h u) ↔ HEq t u := by cases h; rfl
