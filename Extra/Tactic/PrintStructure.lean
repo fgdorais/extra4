8aef1829caf925056d91b3d51e0ab2dea1f1f040
F. G. Dorais <fgdorais@gmail.com>
2024-02-24 14:39:53 -0500

    feat: add options for #print structure

--- Extra/Tactic/PrintStructure.lean
+++ Extra/Tactic/PrintStructure.lean
@@ -1,34 +1,60 @@
-import Std
+
+import Lean.Elab
 
 open Lean Elab Command
 
 /--
 The command `#print structure foo` will print information about the structure `foo`.
 -/
-syntax (name := printStructure) "#print" "structure" ident : command
+syntax (name := printStructure) "#print" "structure" (&"flat" <|> &"full")? ident : command
+
+structure PrintStructureConfig where
+  flat : Bool := false
+  includeSubobjectFields : Bool := true
+
+def strPrintStructure (structName : Name) (cfg : PrintStructureConfig := {}) : MetaM String := do
+  let env ← getEnv
+  if isStructure env structName then
+    let indInfo ← getConstInfoInduct structName
+    let [ctorName] := indInfo.ctors | failure
+    let ctorInfo ← getConstInfoCtor ctorName
+    let levels := if indInfo.levelParams = [] then "" else
+      ".{" ++ ", ".intercalate (indInfo.levelParams.map Name.toString) ++ "}"
+    let mut msg :=
+      s!"structure {structName}{levels} : {← Meta.ppExpr indInfo.type}\n\
+        constructor:\n\
+        {ctorName} : {← Meta.ppExpr ctorInfo.type}\n\
+        fields:\n"
+    let fieldNames :=
+      if cfg.flat then
+        getStructureFieldsFlattened env structName cfg.includeSubobjectFields
+      else
+        getStructureFields env structName
+    for fieldName in fieldNames do
+      if let some structName := findField? env structName fieldName then
+        let projInfo ← getConstInfo (structName ++ fieldName)
+        msg := msg ++ s!"{fieldName} : {← Meta.ppExpr projInfo.type}\n"
+    return msg
+  else
+    throwError "expected structure"
 
 /--
 Implementation for #print structure
 -/
 @[command_elab printStructure] def elabPrintStructure : CommandElab
-| `(#print structure%$tk $id:ident) =>
-  liftTermElabM do
-    if let [name] ← resolveGlobalConst id then
-      let env ← getEnv
-      match getStructureInfo? env name with
-      | none => throwErrorAt tk "expected structure"
-      | some {structName, fieldNames, ..} =>
-        let indInfo ← getConstInfoInduct structName
-        let [ctorName] := indInfo.ctors | failure
-        let ctorInfo ← getConstInfoCtor ctorName
-        let levels := if indInfo.levelParams = [] then "" else
-          ".{" ++ ", ".intercalate (indInfo.levelParams.map Name.toString) ++ "}"
-        let mut msg := s!"structure {structName}{levels} : {← Meta.ppExpr indInfo.type}\n"
-        msg := msg ++ s!"number of parameters: {indInfo.numParams}\n"
-        msg := msg ++ s!"constructor:\n{ctorName} : {← Meta.ppExpr ctorInfo.type}\n"
-        msg := msg ++ s!"projections:\n"
-        for fieldName in fieldNames do
-          let projInfo ← getConstInfo (structName ++ fieldName)
-          msg := msg ++ s!"{structName ++ fieldName} : {← Meta.ppExpr projInfo.type}\n"
+  | `(#print%$tk structure $id:ident) =>
+    liftTermElabM do
+      if let [name] ← resolveGlobalConst id then
+        let msg ← strPrintStructure name
         logInfoAt tk msg
-| _ => throwUnsupportedSyntax
+  | `(#print%$tk structure flat $id:ident) =>
+    liftTermElabM do
+      if let [name] ← resolveGlobalConst id then
+        let msg ← strPrintStructure name ⟨true, false⟩
+        logInfoAt tk msg
+  | `(#print%$tk structure full $id:ident) =>
+    liftTermElabM do
+      if let [name] ← resolveGlobalConst id then
+        let msg ← strPrintStructure name ⟨true, true⟩
+        logInfoAt tk msg
+  | _ => throwUnsupportedSyntax
