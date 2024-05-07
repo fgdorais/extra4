5a67a6a9bb2500424525a0d8d8a119095ad4a953
F. G. Dorais <fgdorais@gmail.com>
2024-05-07 04:32:42 -0400

    chore: std4 -> batteries

--- Extra/Basic.lean
+++ Extra/Basic.lean
@@ -2,7 +2,7 @@
 Copyright © 2023 François G. Dorais. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 -/
-import Std
+import Batteries
 
 /-- Simp attribute for `elim_cast` tactic -/
 register_simp_attr elim_cast
