3a86a4c88a302c7f473b935271b5efa61bf62f19
F. G. Dorais <fgdorais@gmail.com>
2024-07-01 06:23:48 -0400

    feat: better `HEq.funext`

--- Extra/Cast/Basic.lean
+++ Extra/Cast/Basic.lean
@@ -7,11 +7,18 @@ import Extra.Basic
 theorem congr_ndrec {β : α → Sort _} (f : (x : α) → β x → γ) (h : x = x') (y : β x) :
     f x' (Eq.ndrec y h) = f x y := by cases h; rfl
 
-/-- Function extensionality for heterogenous equality -/
-protected theorem HEq.funext {β₁ β₂ : α → Sort _} (f₁ : (x : α) → β₁ x) (f₂ : (x : α) → β₂ x)
+protected theorem HEq.funext₀ {β₁ β₂ : α → Sort _} {f₁ : (x : α) → β₁ x} {f₂ : (x : α) → β₂ x}
     (h : ∀ x, HEq (f₁ x) (f₂ x)) : HEq f₁ f₂ := by
   have : β₁ = β₂ := by funext x; exact type_eq_of_heq (h x)
   cases this
   apply heq_of_eq
   funext x
   exact eq_of_heq (h x)
+
+/-- Function extensionality for heterogenous equality -/
+protected theorem HEq.funext {β₁ : α₁ → Sort _} {β₂ : α₂ → Sort _}
+    {f₁ : (x : α₁) → β₁ x} {f₂ : (x : α₂) → β₂ x} (h : α₁ = α₂)
+    (hf : ∀ x, HEq (f₁ x) (f₂ (h ▸ x))) : HEq f₁ f₂ := by
+  cases h
+  apply HEq.funext₀
+  exact hf
