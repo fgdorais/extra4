c3afbb4dc0f56c72c5bec683432d8cbaa1148a82
fgdorais <fgdorais@users.noreply.github.com>
2025-06-30 03:33:42 +0000

    chore: update toolchain v4.21.0

--- Extra/Vector.lean
+++ Extra/Vector.lean
@@ -7,7 +7,5 @@ namespace Vector
 theorem getElem_eq_getElem_toArray (v : Vector α n) (i) (h : i < n) :
     v[i] = v.toArray[i]'(v.size_toArray.symm ▸ h) := rfl
 
-@[simp] theorem get_eq_getElem (v : Vector α n) (i : Fin n) : v.get i = v[i.val] := rfl
-
 @[simp] theorem uget_eq_getElem (v : Vector α n) (i : USize) (h : i.toNat < n) :
     v.uget i h = v[i.toNat] := rfl
