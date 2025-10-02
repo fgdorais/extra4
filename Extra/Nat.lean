2806a32bd155bba3247a1847109c700de45fa436
F. G. Dorais <fgdorais@gmail.com>
2025-10-02 17:17:27 -0400

    feat: add bitwise lemmas for `Nat`

--- Extra/Nat.lean
+++ Extra/Nat.lean
@@ -1,3 +1,4 @@
+import Extra.Nat.Bit
 import Extra.Nat.Bitwise
 import Extra.Nat.Lemmas
 import Extra.Nat.NeZero
--- Extra/Nat/Bitwise.lean
+++ Extra/Nat/Bitwise.lean
@@ -2,149 +2,81 @@ import Extra.Nat.Lemmas
 
 namespace Nat
 
-@[simp] theorem bit0_div_two (x) : (2 * x) / 2 = x := by omega
+@[simp] theorem and_self_left (a b : Nat) : a &&& (a &&& b) = a &&& b := by
+  apply Nat.eq_of_testBit_eq; simp
 
-@[simp] theorem bit1_div_two (x) : (2 * x + 1) / 2 = x := by omega
+@[simp] theorem and_self_right (a b : Nat) : ((a &&& b) &&& b) = (a &&& b) := by
+  apply Nat.eq_of_testBit_eq; simp
 
-@[simp] theorem bit0_mod_two (x) : (2 * x) % 2 = 0 := by omega
+theorem and_left_comm (x y z : Nat) : x &&& (y &&& z) = y &&& (x &&& z) := by
+  apply Nat.eq_of_testBit_eq; simp [Bool.and_left_comm]
 
-@[simp] theorem bit1_mod_two (x) : (2 * x + 1) % 2 = 1 := by omega
+theorem and_right_comm (x y z : Nat) : (x &&& y) &&& z = (x &&& z) &&& y := by
+  apply Nat.eq_of_testBit_eq; simp [Bool.and_right_comm]
 
-/-! ## Bitwise Recursors -/
+@[simp] theorem or_self_left (a b : Nat) : a ||| (a ||| b) = a ||| b := by
+  apply Nat.eq_of_testBit_eq; simp
 
-@[elab_as_elim]
-protected def recBit {motive : Nat → Sort _}
-    (zero : motive 0)
-    (bit0 : (x : Nat) → motive x → motive (2 * x))
-    (bit1 : (x : Nat) → motive x → motive (2 * x + 1))
-    (x : Nat) : motive x :=
-  if h : x = 0 then h ▸ zero else
-    match h2 : x % 2 with
-    | 0 => Nat.div_add_mod x 2 ▸ h2 ▸ bit0 (x / 2) (Nat.recBit zero bit0 bit1 (x / 2))
-    | 1 => Nat.div_add_mod x 2 ▸ h2 ▸ bit1 (x / 2) (Nat.recBit zero bit0 bit1 (x / 2))
-    | _+2 => False.elim <| by omega
+@[simp] theorem or_self_right (a b : Nat) : (a ||| b) ||| b = a ||| b := by
+  apply Nat.eq_of_testBit_eq; simp
 
-@[elab_as_elim]
-protected def recBitOn {motive : Nat → Sort _} (x : Nat)
-    (zero : motive 0)
-    (bit0 : (x : Nat) → motive x → motive (2 * x))
-    (bit1 : (x : Nat) → motive x → motive (2 * x + 1)) : motive x :=
-  Nat.recBit zero bit0 bit1 x
+theorem or_left_comm (x y z : Nat) : x ||| (y ||| z) = y ||| (x ||| z) := by
+  apply Nat.eq_of_testBit_eq; simp [Bool.or_left_comm]
 
-@[elab_as_elim]
-protected def casesBitOn {motive : Nat → Sort _} (x : Nat)
-    (bit0 : (x : Nat) → motive (2 * x)) (bit1 : (x : Nat) → motive (2 * x + 1)) : motive x :=
-  Nat.recBit (bit0 0) (fun x _ => bit0 x) (fun x _ => bit1 x) x
+theorem or_right_comm (x y z : Nat) : (x ||| y) ||| z = (x ||| z) ||| y := by
+  apply Nat.eq_of_testBit_eq; simp [Bool.or_right_comm]
 
-/-! ## bitwise -/
+theorem and_or_distrib_right (x y z : Nat) : ((x ||| y) &&& z) = x &&& z ||| y &&& z := by
+  apply Nat.eq_of_testBit_eq; simp [Bool.and_or_distrib_right]
 
-@[simp] theorem zero_bitwise (f y) : bitwise f 0 y = bif f false true then y else 0 := by
-  rw [bitwise]; simp
+theorem xor_left_comm (x y z : Nat) : x ^^^ (y ^^^ z) = y ^^^ (x ^^^ z) := by
+  apply Nat.eq_of_testBit_eq; simp only [testBit_xor, Bool.xor_left_comm, implies_true]
 
-@[simp] theorem bitwise_zero (f x) : bitwise f x 0 = bif f true false then x else 0 := by
-  rw [bitwise]; split <;> simp [*]
+theorem xor_right_comm (x y z : Nat) : (x ^^^ y) ^^^ z = (x ^^^ z) ^^^ y := by
+  apply Nat.eq_of_testBit_eq; simp only [testBit_xor, Bool.xor_right_comm, implies_true]
 
-theorem pos_bitwise_pos (f) (hx : x ≠ 0) (hy : y ≠ 0) :
-  bitwise f x y = 2 * bitwise f (x / 2) (y / 2) + bif f (x % 2 = 1) (y % 2 = 1) then 1 else 0 := by
-  rw [bitwise]
-  simp [hx, hy]
-  split <;> simp [*, Nat.two_mul]
+@[simp] theorem xor_xor_cancel_left (x y : Nat) : x ^^^ (x ^^^ y) = y := by
+  apply Nat.eq_of_testBit_eq; simp
 
-theorem bitwise_flip (f x y) : bitwise f x y = bitwise (flip f) y x := by
-  if x = 0 then
-    simp [*, flip]
-  else if y = 0 then
-    simp [*, flip]
-  else
-    simp [*, pos_bitwise_pos, flip, bitwise_flip f (x / 2) (y / 2)]
+@[simp] theorem xor_xor_cancel_right (x y : Nat) : (x ^^^ y) ^^^ y = x := by
+  apply Nat.eq_of_testBit_eq; simp
 
-theorem bit0_bitwise_bit0 (f) (hx : x ≠ 0) (hy : y ≠ 0) :
-    bitwise f (2 * x) (2 * y) = 2 * bitwise f x y + bif f false false then 1 else 0 := by
-  have hx2 : 2 * x ≠ 0 := by intro h; simp [Nat.mul_eq_zero] at h; contradiction
-  have hy2 : 2 * y ≠ 0 := by intro h; simp [Nat.mul_eq_zero] at h; contradiction
-  simp [pos_bitwise_pos _ hx2 hy2]
+theorem eq_of_xor_eq_zero {x y : Nat} : x ^^^ y = 0 → x = y := by
+  intro h; rw [← xor_xor_cancel_left x y, h, xor_zero]
 
-theorem bit1_bitwise_bit0 (f x y) :
-    bitwise f (2 * x + 1) (2 * y) = 2 * bitwise f x y + bif f true false then 1 else 0 := by
-  if y = 0 then
-    have : 2 * y = 0 := by simp [*]
-    simp [*]; cases (f true false) <;> simp [*]
-  else
-    have hy : 2 * y ≠ 0 := by intro h; simp [Nat.mul_eq_zero] at h; contradiction
-    simp [pos_bitwise_pos _ _ hy]
+@[simp] theorem xor_eq_zero_iff {x y : Nat} : x ^^^ y = 0 ↔ x = y :=
+  ⟨eq_of_xor_eq_zero, fun | rfl => Nat.xor_self _⟩
 
-theorem bit0_bitwise_bit1 (f x y) :
-    bitwise f (2 * x) (2 * y + 1) = 2 * bitwise f x y + bif f false true then 1 else 0 := by
-  if x = 0 then
-    have : 2 * x = 0 := by simp [*]
-    simp [*]; cases (f false true) <;> simp [*]
-  else
-    have hx : 2 * x ≠ 0 := by intro h; simp [Nat.mul_eq_zero] at h; contradiction
-    simp [pos_bitwise_pos _ hx]
+theorem xor_ne_zero_iff {x y : Nat} : x ^^^ y ≠ 0 ↔ x ≠ y := by simp
 
-theorem bit1_bitwise_bit1 (f x y) :
-    bitwise f (2 * x + 1) (2 * y + 1) = 2 * bitwise f x y + bif f true true then 1 else 0 := by
-  simp [pos_bitwise_pos]
+theorem xor_right_injective {x : Nat} : Function.Injective (x ^^^ ·) := by
+  intro y z h; rw [← xor_xor_cancel_left x y, ← xor_xor_cancel_left x z]; simp only [h]
 
-/-! ### Bitwise and -/
+theorem xor_left_injective {x : Nat} : Function.Injective (· ^^^ x) := by
+  intro y z h; rw [← xor_xor_cancel_right y x, ← xor_xor_cancel_right z x]; simp only [h]
 
-theorem and_def (x y : Nat) : (x &&& y) = bitwise (· && ·) x y := rfl
+@[simp] theorem xor_right_inj {x y z : Nat} : x ^^^ y = x ^^^ z ↔ y = z :=
+  ⟨(xor_right_injective ·), fun | rfl => rfl⟩
 
-@[local simp] theorem bit0_and_bit0 (x y : Nat) : (2 * x) &&& (2 * y) = 2 * (x &&& y) := by
-  if x = 0 then
-    simp [*, and_def]
-  else if y = 0 then
-    simp [*, and_def]
-  else
-    simp [*, and_def, bit0_bitwise_bit0]
+@[simp] theorem xor_left_inj {x y z : Nat} : x ^^^ z = y ^^^ z ↔ x = y :=
+  ⟨(xor_left_injective ·), fun | rfl => rfl⟩
 
-@[local simp] theorem bit0_and_bit1 (x y : Nat) : (2 * x) &&& (2 * y + 1) = 2 * (x &&& y) := by
-  simp [and_def, bit0_bitwise_bit1]
+theorem and_or_right_injective {m x y : Nat} : x &&& m = y &&& m → x ||| m = y ||| m → x = y := by
+  intro ha ho
+  apply Nat.eq_of_testBit_eq
+  intro i
+  rw [← Bool.and_or_inj_right_iff (m := m.testBit i)]
+  simp [← testBit_and, ← testBit_or, ha, ho]
 
-@[local simp] theorem bit1_and_bit0 (x y : Nat) : (2 * x + 1) &&& (2 * y) = 2 * (x &&& y) :=  by
-  simp [and_def, bit1_bitwise_bit0]
+theorem and_or_right_inj {m x y : Nat} : x &&& m = y &&& m ∧ x ||| m = y ||| m ↔ x = y :=
+  ⟨fun ⟨ha, ho⟩ => and_or_right_injective ha ho, fun | rfl => ⟨rfl, rfl⟩⟩
 
-@[local simp] theorem bit1_and_bit1 (x y : Nat) : (2 * x + 1) &&& (2 * y + 1) = 2 * (x &&& y) + 1 := by
-  simp [and_def, bit1_bitwise_bit1]
+theorem and_or_left_injective {m x y : Nat} : m &&& x = m &&& y → m ||| x = m ||| y → x = y := by
+  intro ha ho
+  apply Nat.eq_of_testBit_eq
+  intro i
+  rw [← Bool.and_or_inj_left_iff (m := m.testBit i)]
+  simp [← testBit_and, ← testBit_or, ha, ho]
 
-/-! ### Bitwise or -/
-
-theorem or_def (x y : Nat) : (x ||| y) = bitwise (· || ·) x y := rfl
-
-@[local simp] theorem bit0_or_bit0 (x y : Nat) : (2 * x) ||| (2 * y) = 2 * (x ||| y) := by
-  if x = 0 then
-    simp [*, or_def]
-  else if y = 0 then
-    simp [*, or_def]
-  else
-    simp [*, or_def, bit0_bitwise_bit0]
-
-@[local simp] theorem bit0_or_bit1 (x y : Nat) : (2 * x) ||| (2 * y + 1) = 2 * (x ||| y) + 1:= by
-  simp [or_def, bit0_bitwise_bit1]
-
-@[local simp] theorem bit1_or_bit0 (x y : Nat) : (2 * x + 1) ||| (2 * y) = 2 * (x ||| y) + 1 := by
-  simp [or_def, bit1_bitwise_bit0]
-
-@[local simp] theorem bit1_or_bit1 (x y : Nat) : (2 * x + 1) ||| (2 * y + 1) = 2 * (x ||| y) + 1 := by
-  simp [or_def, bit1_bitwise_bit1]
-
-/-! ### Bitwise xor -/
-
-theorem xor_def (x y : Nat) : (x ^^^ y) = bitwise Bool.xor x y := rfl
-
-@[local simp] theorem bit0_xor_bit0 (x y : Nat) : (2 * x) ^^^ (2 * y) = 2 * (x ^^^ y) := by
-  if x = 0 then
-    simp [*, xor_def]
-  else if y = 0 then
-    simp [*, xor_def]
-  else
-    simp [*, xor_def, bit0_bitwise_bit0]
-
-@[local simp] theorem bit0_xor_bit1 (x y : Nat) : (2 * x) ^^^ (2 * y + 1) = 2 * (x ^^^ y) + 1 :=  by
-  simp [xor_def, bit0_bitwise_bit1]
-
-@[local simp] theorem bit1_xor_bit0 (x y : Nat) : (2 * x + 1) ^^^ (2 * y) = 2 * (x ^^^ y) + 1 := by
-  simp [xor_def, bit1_bitwise_bit0]
-
-@[local simp] theorem bit1_xor_bit1 (x y : Nat) : (2 * x + 1) ^^^ (2 * y + 1) = 2 * (x ^^^ y) :=  by
-  simp [xor_def, bit1_bitwise_bit1]
+theorem and_or_left_inj {m x y : Nat} : m &&& x = m &&& y ∧ m ||| x = m ||| y ↔ x = y :=
+  ⟨fun ⟨ha, ho⟩ => and_or_left_injective ha ho, fun | rfl => ⟨rfl, rfl⟩⟩
