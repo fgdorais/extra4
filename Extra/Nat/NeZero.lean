2b14182abff3997aa0a5b1a00980903653f5ba40
F. G. Dorais <fgdorais@gmail.com>
2025-10-09 09:35:21 -0400

    fix: remove duplicate `NeZero` instances

--- Extra/Nat/NeZero.lean
+++ Extra/Nat/NeZero.lean
@@ -9,23 +9,9 @@ namespace Nat
 
 def ne_zero (n : Nat) [self : NeZero n] : n ≠ 0 := self.out
 
-def zero_ne (n : Nat) [self : NeZero n] : 0 ≠ n := Ne.symm <| ne_zero n
+def zero_ne (n : Nat) [self : NeZero n] : 0 ≠ n := self.out.symm
 
-def zero_lt (n : Nat) [self : NeZero n] : n > 0 := Nat.zero_lt_of_ne_zero <| ne_zero n
-
-instance (m n : Nat) [NeZero m] : NeZero (m + n) where
-  out h := ne_zero m (Nat.eq_zero_of_add_eq_zero_right h)
-
-instance (m n : Nat) [NeZero n] : NeZero (m + n) where
-  out h := ne_zero n (Nat.eq_zero_of_add_eq_zero_left h)
-
-instance (m n : Nat) [NeZero m] [NeZero n] : NeZero (m * n) where
-  out h := match Nat.mul_eq_zero.mp h with
-    | .inl h => ne_zero m h
-    | .inr h => ne_zero n h
+def zero_lt (n : Nat) [self : NeZero n] : 0 < n := Nat.zero_lt_of_ne_zero <| ne_zero n
 
 instance (n : Nat) : NeZero (n ^ 0) where
   out := Nat.one_ne_zero
-
-instance (m n : Nat) [NeZero m] : NeZero (m ^ n) where
-  out h := ne_zero m (Nat.pow_eq_zero.mp h).1
